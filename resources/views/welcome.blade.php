55381f27ddbfb68d9c17b077e33e5d791e6ea05c
Giyasul Firdaus <158206218+Giyasul@users.noreply.github.com>
2026-01-14 00:51:34 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,6 +54,12 @@
     font-weight:700;
 }
 
+/* indicator bulat */
+.carousel-indicators [data-bs-target]{
+    width:2px;
+    height:2px;
+}
+
 /* ================= SIDEBAR ================= */
 .sidebar-img{
     width:75px;
